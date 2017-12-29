09f847db70b51cf7660901cf0b3b0c3bbf79ccb7
Hara Totapally <htotapally@yahoo.com>
2017-12-29 17:52:55 -0500

    Added new info

---
+++ default/CharOperations.java
@@ -0,0 +1,21 @@
+public class CharOperations {
+
+	public static void main(String[] args) {
+		// Variable Declaration
+		char firstChar;
+		char secondChar;
+		char thirdChar;
+
+		// assign values to variables
+		firstChar = 'f'; // Character values are enclosed between single quotes
+
+		secondChar = 's';
+
+		// Add two characters
+		// The following statement doesnot work
+		// thirdChar = firstChar + secondChar;
+		// Even the following statement doesnot work
+		// as thirdChar is not initialized yet
+		// System.out.println(thirdChar);
+	}
+}
